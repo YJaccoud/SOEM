78353d81d8b919ed15eec76639e3811add454ab0
Yann Jaccoud <yann.jaccoud@anton-paar.com>
2016-06-01 12:49:42 +0200

    MODIFY(osal INtime) : cosmetic update

--- osal/intime/osal.c
+++ osal/intime/osal.c
@@ -52,48 +52,48 @@ static double qpc2usec;
 #define USECS_PER_SEC     1000000
 
 //Internal
-int osal_gettimeofday (struct timeval *tv, void *tz)
+int osal_gettimeofday(struct timeval *tv, void *tz)
 {
    return gettimeofday(tv, tz);   //tz not used
 }
 
 //Date and Time
-ec_timet osal_current_time (void)
+ec_timet osal_current_time(void)
 {
    struct timeval current_time;
    ec_timet return_value;
 
-   osal_gettimeofday (&current_time, 0);
+   osal_gettimeofday(&current_time, 0);
    return_value.sec = current_time.tv_sec;
    return_value.usec = current_time.tv_usec;
    return return_value;
 }
 
-void osal_timer_start (osal_timert * self, uint32 timeout_usec)
+void osal_timer_start(osal_timert * self, uint32 timeout_usec)
 {
    struct timeval start_time;
    struct timeval timeout;
    struct timeval stop_time;
 
-   osal_gettimeofday (&start_time, 0);
+   osal_gettimeofday(&start_time, 0);
    timeout.tv_sec = timeout_usec / USECS_PER_SEC;
    timeout.tv_usec = timeout_usec % USECS_PER_SEC;
-   timeradd (&start_time, &timeout, &stop_time);
+   timeradd(&start_time, &timeout, &stop_time);
 
    self->stop_time.sec = stop_time.tv_sec;
    self->stop_time.usec = stop_time.tv_usec;
 }
 
-boolean osal_timer_is_expired (osal_timert * self)
+boolean osal_timer_is_expired(osal_timert * self)
 {
    struct timeval current_time;
    struct timeval stop_time;
    int is_not_yet_expired;
 
-   osal_gettimeofday (&current_time, 0);
+   osal_gettimeofday(&current_time, 0);
    stop_time.tv_sec = self->stop_time.sec;
    stop_time.tv_usec = self->stop_time.usec;
-   is_not_yet_expired = timercmp (&current_time, &stop_time, <);
+   is_not_yet_expired = timercmp(&current_time, &stop_time, <);
 
    return is_not_yet_expired == FALSE;
 }
