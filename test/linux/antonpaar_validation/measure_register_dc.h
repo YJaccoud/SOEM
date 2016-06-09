0209962433947ad9f25ae8b5b88c4d1c4ed7dcdb
Yann Jaccoud <yann.jaccoud@anton-paar.com>
2016-06-09 17:20:19 +0200

    TMP commit : Only cosmetic (indentation, function name / file name

---
+++ test/linux/antonpaar_validation/measure_register_dc.h
@@ -0,0 +1,26 @@
+#ifndef _EC_MEASURE_REG_H
+#define _EC_MEASURE_REG_H
+
+#include "ethercattype.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define REG_LOCAL_TIME_ADDR         0x910
+#define REG_NEXT_SYNC0_ADDR         0x990
+
+#define EXCEL_FILE_NAME_REG         "C:\\VisualStudioProject\\Reg_Value\\reg_excel_data.csv"
+#define TXT_FILE_NAME_REG           "C:\\VisualStudioProject\\Reg_Value\\reg_txt_data.txt"
+
+#define SAMPLE_NUMBER_BUFFER_REG    60000
+
+void MeasureBufferRegisterDc(uint16 slave);
+void WriteBufferRegisterDc();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _EC_ECATDC_H */
